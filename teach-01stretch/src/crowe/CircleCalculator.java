c435aa6702f379177d44b2100a5ae911fd157f91
hpbcrowe <51143614+hpbcrowe@users.noreply.github.com>
2020-12-22 23:32:07 -0700

    Add files via upload

---
+++ teach-01stretch/src/crowe/CircleCalculator.java
@@ -0,0 +1,35 @@
+package crowe;
+
+import java.util.Scanner;
+
+public class CircleCalculator {
+
+            private static final Scanner _scanner = new Scanner( System.in );
+
+        public double getRadius(){
+            System.out.print("Please enter the radius:");
+            double radius = _scanner.nextDouble();
+            return radius;
+        }
+
+        public void displayCircumference(double radius){
+            double circumference = Math.PI * radius * 2;
+            System.out.format("\tCircumference: %.2f\n", circumference);
+        }
+
+        public void displayArea(double radius){
+            double area = Math.PI * radius * radius;
+
+            System.out.format("\tArea: %.2f\n", area);
+        }
+
+
+
+    public static void main(String[] args) {
+        CircleCalculator calculator = new CircleCalculator();
+        double rad = calculator.getRadius();
+        calculator.displayArea(rad);
+        calculator.displayCircumference(rad);
+	// write your code here
+    }
+}
