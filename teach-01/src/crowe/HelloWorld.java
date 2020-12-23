e5c37ebd63d203fc1c7482ede145ff6b288c252e
hpbcrowe <51143614+hpbcrowe@users.noreply.github.com>
2020-12-22 23:31:44 -0700

    Add files via upload

---
+++ teach-01/src/crowe/HelloWorld.java
@@ -0,0 +1,14 @@
+package crowe;
+
+
+public class HelloWorld {
+
+    public void sayHello(){
+        System.out.println("Hello World!");
+    }
+    public static void main(String[] args) {
+	// write your code here
+        HelloWorld Hi = new HelloWorld();
+        Hi.sayHello();
+    }
+}
