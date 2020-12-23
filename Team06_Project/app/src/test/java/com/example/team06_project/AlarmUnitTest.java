366e8082e69df299c198abbaafe49fb3d9cfa712
hpbcrowe <51143614+hpbcrowe@users.noreply.github.com>
2020-12-22 23:39:52 -0700

    Add files via upload

---
+++ Team06_Project/app/src/test/java/com/example/team06_project/AlarmUnitTest.java
@@ -0,0 +1,25 @@
+package com.example.team06_project;
+
+        import org.junit.Test;
+
+        import java.util.ArrayList;
+
+        import static org.junit.Assert.*;
+
+public class AlarmUnitTest {
+    @Test
+    public void CreatedElement() {
+        assertEquals(false, false);
+    }
+
+    @Test
+    public void AlarmNotificationRegistered() {
+
+    }
+
+    @Test
+    public void AlarmTimeValid() {
+        //Tests that the time is actually valid, in the future.
+    }
+
+}
